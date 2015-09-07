e627054f67c62c8e4032db8868fecf2816341399
Tom Scott <tscott@weblinc.com>
2015-09-07 17:50:04 -0400

    Remove unused job

--- app/jobs/generate_episode_link_job.rb
+++
@@ -1,9 +0,0 @@
-require 'short_link'
-
-class GenerateEpisodeLinkJob < ActiveJob::Base
-  queue_as :short_links
-
-  def perform(episode)
-    episode.update short_url: ShortLink.new(episode).url
-  end
-end
