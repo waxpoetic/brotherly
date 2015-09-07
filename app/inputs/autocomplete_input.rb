28abd2844bc33faae990cd32317d2d24d090efd6
Tom Scott <tscott@weblinc.com>
2015-09-07 18:31:29 -0400

    Autocomplete input fixes

--- app/inputs/autocomplete_input.rb
+++ app/inputs/autocomplete_input.rb
@@ -3,13 +3,9 @@ class AutocompleteInput < SimpleForm::Inputs::Base
     merged_input_options = merge_wrapper_options(
       input_html_options, wrapper_options
     )
-    @builder.concat do
-      @builder.hidden_field attribute_name
-      @builder.text_field_tag "#{attribute_name}_input", merged_input_options
+    template.content_tag :span, class: 'autocomplete', itemprop: attribute_name do
+      template.concat @builder.hidden_field(attribute_name)
+      template.concat @builder.text_field_tag("#{attribute_name}_input", attribute_value, merged_input_options)
     end
-  end
-
-  def input_html_options
-    super.merge class: 'autocomplete', itemprop: attribute_name
   end
 end
