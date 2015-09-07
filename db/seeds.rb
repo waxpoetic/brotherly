bac36b65b3e4c7e69e1992dde4964f19aacacf81
Tom Scott <tscott@weblinc.com>
2015-09-07 17:59:24 -0400

    Update seeds to run regardless

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,20 +2,22 @@ require 'active_record/fixtures'
 
 puts "-- seed '#{Rails.env}'"
 
-if Rails.env.development?
+#if Rails.env.development?
   puts "   add fixtures"
   ActiveRecord::Fixtures.create_fixtures(
     'spec/fixtures', Rails.application.config.seeds
   )
-end
+#end
 
-puts "   add initial admin user"
-User.create!(
-  name: 'admin',
-  email: Rails.application.secrets.admin_email,
-  password: Rails.application.secrets.admin_password,
-  password_confirmation: Rails.application.secrets.admin_password,
-  remember_me: true
-)
+unless User.where(name: 'admin').any?
+  puts "   add initial admin user"
+  User.create!(
+    name: 'admin',
+    email: Rails.application.secrets.admin_email,
+    password: Rails.application.secrets.admin_password,
+    password_confirmation: Rails.application.secrets.admin_password,
+    remember_me: true
+  )
+end
 
 puts "   -> done"
