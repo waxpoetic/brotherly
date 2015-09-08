ecfb4b4b1f57afd20db201b6d8dd5f37dc95319d
Tom Scott <tscott@weblinc.com>
2015-09-07 23:21:45 -0400

    Add :description field to Artist

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150908031758) do
+ActiveRecord::Schema.define(version: 20150908032056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,10 +20,11 @@ ActiveRecord::Schema.define(version: 20150908031758) do
   create_table "artists", force: :cascade do |t|
     t.string   "name"
     t.string   "image_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "slug"
     t.hstore   "links"
+    t.text     "description"
   end
 
   create_table "episodes", force: :cascade do |t|
