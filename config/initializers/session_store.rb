e82e69c11e59ddfcfc5008bbb9fed38f1e8f2f67
Tom Scott <tscott@weblinc.com>
2015-10-25 23:32:23 -0400

    wtf

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,5 @@
 if Rails.env =~ /development|test/
   Rails.application.config.session_store :cookie_store, key: '_brotherly_session'
 else
-  Rails.application.config.session_store :redis_store, \
-    servers: "#{Rails.application.secrets.redis_url}/brotherly-session",
-    expires_in: 90.minutes
+  Rails.application.config.session_store :redis_store, servers: "#{Rails.application.secrets.redis_url}/0/brotherly-session"
 end
