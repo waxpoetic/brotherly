742f9b3479d8bc0a1bcc635e5c70b5c2fd573623
Tom Scott <tscott@weblinc.com>
2015-11-08 22:15:27 -0500

    Remove configure_server block

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,14 +2,4 @@ if defined? Sidekiq
   Sidekiq.configure_client do |config|
     config.redis = { size: 1 }
   end
-
-  Sidekiq.configure_server do |config|
-    database_url = ENV['DATABASE_URL']
-    pool = ENV['MAX_THREADS'].to_i * 7
-
-    if database_url
-      ENV['DATABASE_URL'] = "#{database_url}?pool=#{pool}"
-      ActiveRecord::Base.establish_connection
-    end
-  end
 end
