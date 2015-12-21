6a287f0882173b39cf714c9fd08c757f4a521ce1
Tom Scott <tscott@weblinc.com>
2015-12-21 16:26:50 -0500

    Use CNAMEd CDN host

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -7,5 +7,5 @@ if defined? Refile::S3
   }
   Refile.cache = Refile::S3.new prefix: "cache", **aws
   Refile.store = Refile::S3.new prefix: "store", **aws
-  Refile.cdn_host = "//#{Rails.application.secrets.aws_cloudfront_domain_name}"
+  Refile.cdn_host = '//files.brother.ly'
 end
