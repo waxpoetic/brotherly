90b01d3d35053cfd646c26346f2aacd12fec643c
Tom Scott <tscott@weblinc.com>
2015-12-21 16:09:28 -0500

    Move out of decorator

--- app/decorators/artist_decorator.rb
+++ app/decorators/artist_decorator.rb
@@ -5,7 +5,6 @@ class ArtistDecorator < ApplicationDecorator
     h.attachment_image_tag(
       model,
       :image_file,
-      :fill, 250, 250,
       fallback: "http://placehold.it/250x250?text=#{placeholder_text}",
     )
   end
