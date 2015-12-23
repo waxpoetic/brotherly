d3e2168255839ed6ce69cef69aa004a3ca13f607
Tom Scott <tscott@weblinc.com>
2015-12-23 01:37:59 -0500

    Remove Brotherly::Dotenv and the deploy tasks

--- lib/brotherly.rb
+++ lib/brotherly.rb
@@ -1,8 +1,9 @@
 require 'active_support/dependencies/autoload'
 
+# Library code that supports the rest of the application, including
+# custom base classes and other functionality.
 module Brotherly
   extend ActiveSupport::Autoload
 
   autoload :Service
-  autoload :Dotenv
 end
