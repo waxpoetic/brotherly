4c62d2b8b803b881bc178ce329001a8101eae0f6
Tom Scott <tscott@weblinc.com>
2015-12-23 01:28:53 -0500

    Remove custom versioning, use semver in the future

--- lib/brotherly.rb
+++ lib/brotherly.rb
@@ -1,16 +1,8 @@
-require 'brotherly/version'
 require 'active_support/dependencies/autoload'
 
 module Brotherly
   extend ActiveSupport::Autoload
 
-  autoload :Responder
-  autoload :ExposureStrategy
   autoload :Service
-  autoload :ShortLink
   autoload :Dotenv
-
-  def self.version
-    VERSION
-  end
 end
