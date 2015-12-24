21964eb72ed87b3e656b5c59392e0c20ae27a6fd
Tom Scott <tscott@weblinc.com>
2015-12-24 18:04:48 -0500

    Fix airbrake init

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,3 +1,3 @@
-Airbrake.configure do |config|
-  config.api_key = Rails.application.secrets.airbrake_api_key
-end if Rails.env.production?
+if defined? Airbrake
+  Airbrake.api_key = Rails.application.secrets.airbrake_api_key
+end
