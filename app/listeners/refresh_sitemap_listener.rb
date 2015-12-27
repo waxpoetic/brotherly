b6a9296ef126b88205f698396bb2246d49870907
Tom Scott <tscott@weblinc.com>
2015-12-27 03:25:11 -0500

    Only generate sitemaps in production

--- app/listeners/refresh_sitemap_listener.rb
+++ app/listeners/refresh_sitemap_listener.rb
@@ -2,6 +2,7 @@ class RefreshSitemapListener < Brotherly::Listener
   on :created, :destroyed
 
   def perform
+    return unless Rails.env.production?
     load Rails.root.join('config', 'sitemap.rb')
     SitemapGenerator::Sitemap.ping_search_engines
   end
