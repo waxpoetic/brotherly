c645d8e11437665b7e796d1f6c3c0c87ab58bc7f
Tom Scott <tscott@weblinc.com>
2015-12-27 18:24:58 -0500

    Fix configuration causing image issues

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -3,9 +3,9 @@ if defined? Refile::S3
     access_key_id: Rails.application.secrets.aws_access_key_id,
     secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: Rails.application.secrets.aws_region,
-    bucket: Rails.application.secrets.aws_s3_bucket_name
+    bucket: Rails.application.secrets.cdn_domain_name
   }
   Refile.cache = Refile::S3.new prefix: 'cache', **aws
   Refile.store = Refile::S3.new prefix: 'store', **aws
-  Refile.cdn_host = Rails.application.secrets.cdn_domain_name
+  Refile.cdn_host = '//' + Rails.application.secrets.cdn_domain_name
 end
