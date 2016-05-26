c5d286b1e4f0140fcb2f6881f58b0dcf7ee88caa
Tom Scott <tscott@weblinc.com>
2016-05-26 12:54:47 -0400

    Remove pipeline config from Transcode job

--- app/jobs/transcode_video_job.rb
+++ app/jobs/transcode_video_job.rb
@@ -2,9 +2,7 @@ class TranscodeVideoJob < ActiveJob::Base
   queue_as :default
 
   def perform(episode)
-    return unless Transcode.create(
-      pipeline: :video, input: episode.video_file_url
-    )
-    episode.update video_transcoded_at: Time.zone.now
+    return unless Transcode.create input: episode.video_file_url
+    episode.update video_transcoded_at: Time.current
   end
 end
