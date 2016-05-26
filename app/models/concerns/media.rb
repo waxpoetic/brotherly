a738e8b64fc7877a7f9021c54834423a4a1f6deb
Tom Scott <tscott@weblinc.com>
2016-05-26 12:53:29 -0400

    Remove audio transcoding

--- app/models/concerns/media.rb
+++ app/models/concerns/media.rb
@@ -11,20 +11,11 @@ module Media
     attachment :audio_file, extension: AUDIO_EXTENSIONS
     attachment :video_file, extension: VIDEO_EXTENSIONS
 
-    after_save :transcode_audio!, if: :needs_audio_transcode?
     after_save :transcode_video!, if: :needs_video_transcode?
-  end
-
-  def needs_audio_transcode?
-    audio_file_id_changed? || !audio_transcoded?
   end
 
   def needs_video_transcode?
     video_file_id_changed? || !video_transcoded?
-  end
-
-  def audio_transcoded?
-    audio_transcoded_at.present?
   end
 
   def video_transcoded?
@@ -33,19 +24,7 @@ module Media
 
   protected
 
-  def transcode_audio!
-    if audio_file_url =~ /\.mp3\Z/
-      episode.update transcoded_audio_at: Time.zone.now
-    else
-      TranscodeAudioJob.perform_later self
-    end
-  end
-
   def transcode_video!
-    if video_file_url =~ /\.m3u8\Z/
-      episode.update transcoded_video_at: Time.zone.now
-    else
-      TranscodeVideoJob.perform_later self
-    end
+    TranscodeVideoJob.perform_later self
   end
 end
