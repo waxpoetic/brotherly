5922079a2bb41a3da32e1195ecc0c0890e3f2640
Tom Scott <tscott@weblinc.com>
2016-07-22 20:26:52 -0400

    Fix bug that i should have caught in testing

--- app/jobs/transcode_video_job.rb
+++ app/jobs/transcode_video_job.rb
@@ -2,7 +2,7 @@ class TranscodeVideoJob < ActiveJob::Base
   queue_as :default
 
   def perform(episode)
-    transcode = Transcode.new episode.video_file, episode.playlist_name
+    transcode = Transcode.new episode.video_file_id, episode.playlist_name
     episode.update video_transcoded_at: Time.current if transcode.save
   end
 end
