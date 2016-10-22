ab7fb9509fb0d9ef1aeef32c184e3d889fbf8b23
Tom Scott <tscott@weblinc.com>
2016-10-22 18:46:19 -0400

    calling it a night

--- app/helpers/metadata_helper.rb
+++ app/helpers/metadata_helper.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 module MetadataHelper
   def logo_image_url
-    image_url(
-      'logo.jpeg',
-      host: Rails.configuration.action_controller.asset_host
-    )
+    image_url 'logo.jpg'
   end
 
   def meta_keywords
