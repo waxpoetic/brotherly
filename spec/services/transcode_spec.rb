37875f259edbcaa33a49818249bd26dd79b2b0a1
Tom Scott <tscott@weblinc.com>
2016-12-18 23:12:20 -0500

    Fix broken unit tests

--- spec/services/transcode_spec.rb
+++ spec/services/transcode_spec.rb
@@ -4,13 +4,13 @@ require './app/services/transcode'
 
 RSpec.describe Transcode, type: :service do
   subject do
-    described_class.new SecureRandom.hex, 'episode-name'
+    described_class.new 'raw-videos/Episode Name.mp4'
   end
 
   it 'turns presets into output params' do
     output = subject.outputs.first
     expect(output).to be_a(Hash)
-    expect(output[:key]).to eq(Transcode::VIDEO_PRESETS.keys.first)
+    expect(output[:key]).to eq('episode-name-400k')
     expect(output[:preset_id]).to eq(Transcode::VIDEO_PRESETS.values.first)
     expect(output[:segment_duration]).to eq(Transcode::SEGMENT_DURATION)
   end
@@ -18,14 +18,12 @@ RSpec.describe Transcode, type: :service do
   it 'builds playlist params' do
     expect(subject.playlist[:name]).to eq subject.name
     expect(subject.playlist[:format]).to eq Transcode::FORMAT
-    expect(subject.playlist[:output_keys]).to eq(Transcode::VIDEO_PRESETS.keys)
+    expect(subject.playlist[:output_keys]).to eq(subject.output_keys)
   end
 
   it 'builds full set of attributes' do
-    expect(subject.attributes[:pipeline_id]).to eq(
-      Rails.application.secrets.aws_transcoder_pipeline_id
-    )
-    expect(subject.attributes[:input][:key]).to eq(subject.input)
+    expect(subject.attributes[:pipeline_id]).to be_present
+    expect(subject.attributes[:input][:key]).to eq(subject.id)
     expect(subject.attributes[:output_key_prefix]).to eq(subject.output_prefix)
     expect(subject.attributes[:outputs]).to eq(subject.outputs)
     expect(subject.attributes[:playlists]).to include(subject.playlist)
