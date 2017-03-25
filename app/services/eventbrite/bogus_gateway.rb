99b8d3921e7bd7963197ff62d28f110bcff1c18d
Tom Scott <tscott@weblinc.com>
2017-03-25 10:05:21 -0400

    Remove Eventbrite code. We may use this later but its not in use right now.

--- app/services/eventbrite/bogus_gateway.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-module Eventbrite
-  class BogusGateway < Gateway
-    def create_event(event)
-      Response.new id: SecureRandom.random_number(5), event: event
-    end
-  end
-end
