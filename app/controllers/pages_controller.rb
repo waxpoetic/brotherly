8633fecdfda5f72465c02fb72bd6fd3a53609dc6
Tom Scott <tscott@weblinc.com>
2017-03-25 10:39:58 -0400

    Add past episodes to slideshow

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   def home
     @current_episodes = EpisodesPresenter.new Episode.current
     @upcoming_episodes = EpisodesPresenter.new Episode.upcoming
+    @past_episodes = EpisodesPresenter.new Episode.recent.limit(5)
     # TODO: Article.featured
     @features = Episode.featured + Performance.featured
   end
