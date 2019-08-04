2c1e0efd6298996efcaf6ff7dc5b9908f44dd72e
Tom Scott <tscott@weblinc.com>
2019-08-03 20:50:43 -0400

    Block Public Access to Staging Server
    - Add Basic HTTP auth to ApplicationController
    - Remove static `robots.txt` and replace with dynamic route that
      disallows all when basic auth is provided.
    - Update PagesController spec to be a request spec, and remove
      legacy controller testing gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,10 @@ class ApplicationController < ActionController::Base
 
   # Don't render the layout when serving an Ajax request.
   layout :use_layout?
+
   DEFAULT_LAYOUT = 'application'
+  HTTP_AUTH_USERNAME = ENV['HTTP_AUTH_USERNAME']
+  HTTP_AUTH_PASSWORD = ENV['HTTP_AUTH_PASSWORD']
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -22,8 +25,19 @@ class ApplicationController < ActionController::Base
   halt Event::NotFoundError, with: :not_found
 
   before_action :cache_page, if: -> { request.get? }
+  before_action :http_authenticate!, if: :http_auth?
 
   protected
+
+  def http_auth?
+    HTTP_AUTH_USERNAME.present? && HTTP_AUTH_PASSWORD.present?
+  end
+
+  def http_authenticate!
+    authenticate_or_request_with_http_basic 'brother.ly' do |username, password|
+      username == HTTP_AUTH_USERNAME && password == HTTP_AUTH_PASSWORD
+    end
+  end
 
   def use_layout?
     request.xhr? ? false : DEFAULT_LAYOUT
