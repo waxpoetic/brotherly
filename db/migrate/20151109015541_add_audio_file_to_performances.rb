57c327462c4512bd20b519812208face273adb91
Tom Scott <tscott@weblinc.com>
2019-09-28 11:48:01 -0400

    Update schema and migrations

--- db/migrate/20151109015541_add_audio_file_to_performances.rb
+++ db/migrate/20151109015541_add_audio_file_to_performances.rb
@@ -5,6 +5,5 @@ class AddAudioFileToPerformances < ActiveRecord::Migration[5.0]
     remove_column :performances, :audio_recording_id
     remove_column :performances, :video_recording_id
     add_column :performances, :audio_file_id, :string
-    add_column :performances, :video_file_id, :string
   end
 end
